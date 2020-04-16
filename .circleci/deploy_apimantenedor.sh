80126ead2cdc55746084256245bafae573f514b8
giova <giovanni.aravena@avalora.com>
2020-04-16 07:27:02 -0400

    Actualización Servicios & Modelo

--- .circleci/deploy_apimantenedor.sh
+++ .circleci/deploy_apimantenedor.sh
@@ -2,7 +2,10 @@ cd /home/ohl/controlCamionesApi/apimantenedor
 git checkout develop
 git pull
 cd /home/ohl/controlCamionesApi/
-docker-compose -f docker-compose.prod.yml up -d --build
+docker-compose -f docker-compose.prod.yml stop apiman
+docker-compose -f docker-compose.prod.yml build apiman
+docker-compose -f docker-compose.prod.yml up -d
+# docker-compose -f docker-compose.prod.yml up -d --build
 #docker-compose -f docker-compose.prod.yml exec web python manage.py migrate --noinput
 #docker-compose -f docker-compose.prod.yml exec web python manage.py makemigrations --noinput
 #docker-compose -f docker-compose.prod.yml exec web python manage.py collectstatic --no-input --clear
